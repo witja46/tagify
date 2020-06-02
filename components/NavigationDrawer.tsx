96e69ffd0c073a26621bbfe89218c0c60647fcb3
Jacob Bachmann <jacob.bachmann@posteo.de>
2020-06-02 23:54:31 +0200

    Created React component NavigationDrawer

---
+++ components/NavigationDrawer.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+// import { SwipeableDrawer } from "@material-ui/core";
+
+export const NavigationDrawer = () => {
+  return (
+    <>
+      {/* <SwipeableDrawer> */}
+
+      {/* </SwipeableDrawer> */}
+    </>
+  );
+}

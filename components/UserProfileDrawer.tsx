858b5b7b1b50b0e85a904df177993152f623d5cb
Jacob Bachmann <jacob.bachmann@posteo.de>
2020-06-02 23:54:50 +0200

    Created React component UserProfileDrawer

---
+++ components/UserProfileDrawer.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+// import { SwipeableDrawer } from "@material-ui/core";
+
+export const UserProfileDrawer = () => {
+  return (
+    <>
+      {/* <SwipeableDrawer> */}
+
+      {/* </SwipeableDrawer> */}
+    </>
+  );
+}
